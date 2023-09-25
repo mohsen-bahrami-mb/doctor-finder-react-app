a91fa4e6bc06bce109cb575be002e9b51b64947e
mohsen <mohsen.bahrami7690@gmail.com>
2023-09-25 08:08:24 +0330

    fix confilit

--- src/index.js
+++ src/index.js
@@ -7,15 +7,9 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
->>>>>>> c125d013098694fa3017597e16e3c2d5a44f3d18
 );
